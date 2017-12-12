4849a519cbd2406389e0e473d62e167a06699048
yibh <yibhgb@gmail.com>
2017-12-12 18:37:45 +0800

    init

---
+++ app/src/main/java/com/wfy/cola/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.wfy.cola
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val context = MyApp.getContext()
+//        Log.w("MainActivity",context)
+    }
+}
